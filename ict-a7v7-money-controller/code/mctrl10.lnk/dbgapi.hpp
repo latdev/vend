c0f517404e8f18dd42419f72ea946ce0ad14415e
TuXAPuK <TuXAPuK@bitbucket.org>
2017-12-08 12:37:10 +0200

    Basic uncompleted version

--- ict-a7v7-money-controller/code/mctrl10.lnk/dbgapi.hpp
+++ ict-a7v7-money-controller/code/mctrl10.lnk/dbgapi.hpp
@@ -1,3 +1,24 @@
+#include <Arduino.h>
 
+// if debug mode is enabled, then enable arduino serial
+#if defined(DEBUG) and DEBUG == 1
+  #define serialBegin() Serial.begin(9600); while(!Serial)
+  #define dbg(...) Serial.print(String("") + __VA_ARGS__)
+  #define dbgln(...) Serial.println(String("") + __VA_ARGS__)
+#else
+  #if not defined(DEBUG)
+    #warning DEBUG Constant not defined, debugger disabled!
+  #endif
+  #define serialBegin()
+  #define dbg(...)
+  #define dbgln(...)
+#endif
 
+// Code folding hack for Arduino Studio
+// folding begin :  tree({ Foldind comment )
+// folding end   :  tree(})
+#define tree(...)
+
+// Standart callback function typezation
+typedef void (*callback)(void);
 
--- ict-a7v7-money-controller/code/mctrl10.lnk/mctrl10.lnk.ino
+++ ict-a7v7-money-controller/code/mctrl10.lnk/mctrl10.lnk.ino
@@ -1,10 +1,23 @@
 #define DEBUG 1
 #include "dbgapi.hpp"
 
-void setup() {
-  // put your setup code here, to run once:
-}
+const PIN_INHIBIT_CONTROL uint8_t = 5
+const INT_CREDIT_INPUT    uint8_t = 0
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+tree({ Arduino main procedures )
+
+  void setup() {
+    pinMode(PIN_INHIBIT_CONTROL, OUTPUT);
+    digitalWrite(PIN_INHIBIT_CONTROL, LOW);
+    serialBegin();
+    dbgln("Latdev ICT A7/V7 Controller");
+
+
+    digitalWrite(PIN_INHIBIT_CONTROL, HIGH);
+  }
+  
+  void loop() {
+    /**/
+  }
+
+tree(})
